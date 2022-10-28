599c8b60c76a2e2426e9827027cfbfa00d17ff30
Ayush Yadav <ayush020yv@gmail.com>
2022-10-28 20:49:18 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import youtube from './api/youtube';
+import { Grid } from "@material-ui/core";
+import SearchBar from './components/SearchBar';
+import { useState } from 'react';
+import VideoDetail from './components/VideoDetail';
+import VideoList from  './components/VideoList';
+
+
 
 function App() {
+ const [videos, setVideos] = useState([]);
+ const [selectedVideo, setSelectedVideos] = useState({ id:{}, snippet:{} });
+
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid style={{ justifyContent: "center" }} container spacinf={10}>
+        <Grid item xs={11}>
+          <Grid container space={10}>
+
+            <Grid item xs={12}>
+              <SearchBar onSubmit = {handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail videos={selectedVideo}/>
+            </Grid>
+            <Grid item xs={4}>
+             < VideoList  videos={videos} onVideoSelect={setSelectedVideos}/>
+            </Grid>
+          
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
+  async function handleSubmit(searchItem) {
+    const {data : {items:videos}} = await youtube.get("search",{
+      params: {
+        part:"snippet",
+        maxResults:5,
+        key:"AIzaSyBldCqK4VSk_a4igR0Ia40N5f58MTsVZVE",
+        // "AIzaSyBldCqK4VSk_a4igR0Ia40N5f58MTsVZVE",
+        q: searchItem,
+
+      }
+    });
+    setVideos(videos);
+    setSelectedVideos(videos[0]);
+ 
+  }
 }
 
 export default App;
---
+++ src/api/youtube.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURl: "https://www.googleapis.com/youtube/v3",
+})
+
+// AIzaSyCMF61S2-cmiH3Nw3l8HHoH2insIDDluKk
